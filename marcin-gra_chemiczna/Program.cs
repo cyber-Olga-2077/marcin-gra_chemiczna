d755268c173862027f5ed18b90ddc6110ae4cc30
amakazor <amakazor@gmail.com>
2019-12-23 18:17:52 +0100

    init

---
+++ marcin-gra_chemiczna/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marcin_gra_chemiczna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
